3f1d313c4c4763f8c630c509f3f1072bfc42e37f
jonasrmarques <jonas.marques.ip@gmail.com>
2024-10-11 14:24:22 -0300

    feat: consertado o nav e implementado os cards
    Foi consertado o problema de quebra de margem na navbar e foi implementado os 3 cards iniciais da página inicial.

---
+++ front-end/naveguardFront/src/components/MainContent/Cards/Cards.jsx
@@ -0,0 +1,38 @@
+import './Cards.css'
+
+function Cards(){
+    return(
+        <div className = "cards">
+            
+            <div className="first_card">
+                <div className="header_card_one">
+                    PARA TODOS
+                </div>
+                <div className="card_content_one">
+                    Conteúdo especial para os responsáveis, com dicas e orientações para manter a navegação da criança sempre segura.
+                </div>
+            </div>
+
+            <div className="second_card">
+                <div className="header_card_two">
+                    DIVERSÃO E SEGURANÇA
+                </div>
+                <div className="card_content_two">
+                    Com jogos interativos, a criança  aprende a identificar e evitar riscos online de forma divertida e envolvente.
+                </div>
+            </div>
+
+            <div className="third_card">
+                <div className="header_card_three">
+                    PROTEÇÃO GARANTIDA
+                </div>
+                <div className="card_content_three">
+                    A criança navega em um ambiente seguro e educativo enquanto aprende sobre os perigos da internet e como evitá-los
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Cards
