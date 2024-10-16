5321635851eaf79f64c639743e4aad279409d2d3
Antonio.lopes <antonyejoao@gmail.com>
2024-10-16 18:20:21 -0300

    Verificando conflito da branch Antonio e Jonas após o pull

--- front-end/naveguardFront/src/App.jsx
+++ front-end/naveguardFront/src/App.jsx
@@ -1,26 +1,16 @@
 // import { useState } from 'react'
-import Home from './pages/Home.jsx'
 import './App.css'
-<<<<<<< HEAD
-import Header from './components/header/Header'
+import Header from './components/Header/Header.jsx'
 import MainContent from './components/MainContent/MainContent'
-=======
-
->>>>>>> 5b1b3b3233d819cb7a5786ff2c4d243e81f7f413
 
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <Header />
       <MainContent/>
-      <Footer/>
       {/*<SignatureCard/> */}
-=======
-      <Home />
->>>>>>> 5b1b3b3233d819cb7a5786ff2c4d243e81f7f413
     </>
   )
 }
--- front-end/naveguardFront/src/components/MainContent/MainContent.jsx
+++ front-end/naveguardFront/src/components/MainContent/MainContent.jsx
@@ -1,8 +1,7 @@
 import Section from "./Section/Section"
 import Cards from "./Cards/Cards"
 import Island from "./Island/Island"
-import Decorations from "../footer/OceanDecorations/OceanDecorations"
-import Footer from "../footer/Footer"
+
 
 function MainContent(){
     return(
