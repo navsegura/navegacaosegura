55b94963dfa50db32d27da5544ff9e9e6bf65fc5
Antonio.lopes <antonyejoao@gmail.com>
2024-10-16 01:24:44 -0300

    add footer content and ocean decorations

--- front-end/naveguardFront/src/components/footer/Footer.jsx
+++ front-end/naveguardFront/src/components/footer/Footer.jsx
@@ -1,44 +1,15 @@
 import "./Footer.css";
 import { Images } from "../../assets/images.jsx";
+import OceanDecorations from "./OceanDecorations/OceanDecorations.jsx";
+import FooterContent from "./FooterContent/FooterContent.jsx";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className="conteudo-geral">
-          <img src={Images.Footer} />
-
-          <div className="conteudo-geral-colunas">
-            <div className="main-titulo">
-              <h1 className="h1-footer">NAVEGUARD</h1>
-            </div>
-            <div className="primeira-coluna">
-              <p>Naveguard &copy; 2024</p>
-              <p>Todos os direitos reservados</p>
-            </div>
-
-            <div className="segunda-coluna">
-              <p>Home</p>
-              <p>Sobre</p>
-              <p>Apoie o Naveguard</p>
-              <p>Fale conosco</p>
-            </div>
-
-            <div className="terceira-coluna">
-              <p>Política de privacidade</p>
-              <p>Termos de uso</p>
-            </div>
-
-            <div className="quarta-coluna">
-              <p>Receba Novidades</p>
-              <input
-                className="input-email"
-                type="email"
-                placeholder="Insira seu e-mail"
-              />
-            </div>
-          </div>
-        </div>
+          <OceanDecorations/>
+          <FooterContent/>
+          <img className="Footer-image" src={Images.Footer} alt="" width={'100%'} />
       </footer>
     </>
   );
