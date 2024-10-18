31b26e0efcaeac33378ada0cc5d70515a5fb9147
Antonio.lopes <antonyejoao@gmail.com>
2024-10-18 17:25:05 -0300

    criando signature cards

--- front-end/naveguardFront/src/App.jsx
+++ front-end/naveguardFront/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header from './components/Header/Header.jsx'
 import MainContent from './components/MainContent/MainContent'
+import SignatureCard from './components/SignatureCards/SignatureCard.jsx'
 import Footer from './components/footer/Footer.jsx'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <MainContent/>
+      <SignatureCard/>
       <Footer/>
     </>
   )
