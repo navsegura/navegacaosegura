8a9e5aa71c6184d556a3b95f5cc541df61f4641c
jonasrmarques <jonas.marques.ip@gmail.com>
2024-10-28 23:37:05 -0300

    feat: replaced the css of the section
    In the previous merge, I accidentally deleted the css from the section, I replaced it

--- front-end/naveguardFront/src/components/MainContent/Section/Section.jsx
+++ front-end/naveguardFront/src/components/MainContent/Section/Section.jsx
@@ -1,4 +1,4 @@
-import "./Section.css";
+import './Section.css'
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Images } from "../../../assets/images";
---
+++ front-end/naveguardFront/src/routes/PrivateRoute.jsx
@@ -0,0 +1,7 @@
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ element, isLoggedIn }) => {
+    return isLoggedIn ? element : <Navigate to="/login" />; 
+};
+
+export default PrivateRoute;
