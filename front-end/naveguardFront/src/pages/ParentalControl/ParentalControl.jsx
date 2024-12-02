28bf81f29dcc900052c41894d1705b8f4206e3f3
wataredev <158059889+wataredev@users.noreply.github.com>
2024-12-02 00:04:16 -0300

    fix: video bug

--- front-end/naveguardFront/src/pages/ParentalControl/ParentalControl.jsx
+++ front-end/naveguardFront/src/pages/ParentalControl/ParentalControl.jsx
@@ -59,7 +59,7 @@ const ParentalControl = () => {
                     <Img src={Images.KidLogger} alt="" />
                     </MolduraImg>
                 <Description>Ferramenta simples que permite monitorar a atividade das crianças em dispositivos móveis e computadores.</Description>
-                <button onClick={() => handleVideoClick("https://youtu.be/6UIvMi5xYXU?si=DVUwcvjP_SHFN-PJ")}>Assistir</button>
+                <button onClick={() => handleVideoClick(videos.kidLogger)}>Assistir</button>
                 </Card>
                 <Card>
                   <MolduraImg>
