57a0b27809c1df10f30edc0acd97336f4f6be3ee
wataredev <158059889+wataredev@users.noreply.github.com>
2024-12-08 23:14:11 -0300

    fix: utils

--- front-end/naveguardFront/src/utils/system.js
+++ front-end/naveguardFront/src/utils/system.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://naveguard-api-fbf2e980cc0b.herokuapp.com/";
 export const TOKEN_KEY = "com.naveguard/Token";
-export const CLIENT_ID = "myclientid";
-export const CLIENT_SECRET = "myclientsecret";
+export const CLIENT_ID = "naveguardid";
+export const CLIENT_SECRET = "naveguardsecret";
